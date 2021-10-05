71ad94d3999a0ce9a6ecf44800628eae6b2c6ead
iceeye2017 <johannes.h2003@gmail.com>
2021-10-05 20:19:42 +0200

    First steps for homepage

---
+++ scripts/src.head.php
@@ -0,0 +1,8 @@
+
+<head>
+  <meta charset="utf-8">
+  <title>Picture-Forum</title>
+
+  <link rel="stylesheet" href="inc/css/home.css">
+
+</head>
---
+++ scripts/src.home.php
@@ -0,0 +1,26 @@
+<body>
+
+    <header>
+        
+        <div class = "title">
+
+            <h1>Picture Forum</h1>
+
+        </div>
+
+        <div class = "navigator">
+
+            <ul class = "categories">
+
+                <li><div class="celements"><h3>Home</h3></div></li>
+                <li><div class="celements"><h3>Discover</h3></div></li>
+                <li><div class="celements"><h3>Contact</h3></div></li>
+
+            </ul>
+
+        </div>
+
+    </header>
+
+
+</body>
