03ca45f0ef2530443aedce844aab7715816fcdac
iceeye2017 <johannes.h2003@gmail.com>
2021-10-05 20:59:30 +0200

    Some test tries

--- scripts/src.home.php
+++ scripts/src.home.php
@@ -8,15 +8,22 @@
 
         </div>
 
-        <div class = "navigator">
+        <div class = "topnavigator">
 
-            <ul class = "categories">
+            <div class = "navigator">
+                
+                <ul class = "categories">
 
-                <li><div class="celements"><h3>Home</h3></div></li>
-                <li><div class="celements"><h3>Discover</h3></div></li>
-                <li><div class="celements"><h3>Contact</h3></div></li>
+                    <li><div class="celements"><h3>Home</h3></div></li>
+                    <li><div class="celements"><h3>Discover</h3></div></li>
+                    <li><div class="celements"><h3>About</h3></div></li>
+                    <li><div class="celements"><h3>Contact</h3></div></li>
 
-            </ul>
+                </ul>
+            
+            </div>
+
+            <div class="searchbar"><input type="text" placeholder="Search.."></div>
 
         </div>
 
