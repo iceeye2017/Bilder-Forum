f6f651a3501b799570677f140c319a69ddd0e888
iceeye2017 <johannes.h2003@gmail.com>
2021-10-06 15:14:30 +0200

    Maggior delets

--- scripts/src.head.php
+++ scripts/src.head.php
@@ -3,6 +3,7 @@
   <meta charset="utf-8">
   <title>Picture-Forum</title>
 
+  <link rel="stylesheet" href="inc/css/reset.css">
   <link rel="stylesheet" href="inc/css/home.css">
 
 </head>
--- scripts/src.home.php
+++ scripts/src.home.php
@@ -8,22 +8,16 @@
 
         </div>
 
-        <div class = "topnavigator">
-
-            <div class = "navigator">
+        <div class = "navigator">
                 
-                <ul class = "categories">
+            <ul>
 
-                    <li><div class="celements"><h3>Home</h3></div></li>
-                    <li><div class="celements"><h3>Discover</h3></div></li>
-                    <li><div class="celements"><h3>About</h3></div></li>
-                    <li><div class="celements"><h3>Contact</h3></div></li>
+                <li class = "navelements">Home</li>
+                <li class = "navelements">Discover</li>
+                <li class = "navelements">About</li>
+                <li class = "navelements">Contact</li>
 
-                </ul>
-            
-            </div>
-
-            <div class="searchbar"><input type="text" placeholder="Search.."></div>
+            </ul>
 
         </div>
 
