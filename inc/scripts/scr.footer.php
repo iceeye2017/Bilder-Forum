6d959018f47dea6f6114fdc28f9a325e4f16a742
iceeye2017 <johannes.h2003@gmail.com>
2021-10-11 15:44:34 +0200

    File upload button for settings/profile

--- inc/scripts/scr.footer.php
+++ inc/scripts/scr.footer.php
@@ -2,3 +2,5 @@
 
 <script src="inc/js/head.js"></script>
 
+<script src="inc/js/setting.js"></script>
+
--- inc/scripts/scr.setting.php
+++ inc/scripts/scr.setting.php
@@ -4,6 +4,17 @@
 
         <img src="https://www.inixgroup.com/wp-content/uploads/2020/12/placeholder.png" alt="Profile Picture"/>
 
+        
+        <div id="profileImage" class="p-image avatar">
+            <i class="fa fa-camera upload-button"></i>
+
+            <form name="profileForm" method="post" action="#">
+                <input id="imageUpload" class="file-upload" type="file" accept="image/*"/>
+            </form>
+
+        </div>
+     </div>
+
     </div>
 
     <ul class="userInformation">
