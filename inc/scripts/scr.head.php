072db2f586d6efdb3485008f377a089e41573a7e
SmallCodee <hannesasd@gmail.com>
2021-10-14 09:18:08 +0200

    fix: Removed old css imports

--- inc/scripts/scr.head.php
+++ inc/scripts/scr.head.php
@@ -7,8 +7,6 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
   
-  <link rel="stylesheet" href="./inc/css/reset.css">
-  <link rel="stylesheet" href="./inc/css/head.css">
   <link rel="stylesheet" href="inc/css/reset.css">
   <link rel="stylesheet" href="inc/css/nav.css">
   <link rel="stylesheet" href="inc/css/profile.css">
