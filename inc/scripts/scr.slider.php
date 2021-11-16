27a1405043578cccdb9a68cfe6566eba5902afd5
hannest03 <hannesasd@gmail.com>
2021-11-16 13:49:36 +0100

    feat: Improved slider

--- inc/scripts/scr.slider.php
+++ inc/scripts/scr.slider.php
@@ -4,6 +4,7 @@
     <div class="slideshow-container">
         <div class="slide">
             <img src="https://images.news18.com/ibnlive/uploads/2021/06/1622715559_disha.jpg" style="width:100%">
+            <p>Hannes</p>
         </div>
         <div class="slide">
             <img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=" style="width:100%">
